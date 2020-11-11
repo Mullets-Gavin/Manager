3d837d66e912081ed45b5fc6877401d727019707
Gavin Rosenthal <38087728+Mullets-Gavin@users.noreply.github.com>
2020-11-11 09:49:26 -0500

    code polish & new additional functions

--- src/ReplicatedStorage/DiceManager/init.lua
+++ src/ReplicatedStorage/DiceManager/init.lua
@@ -8,22 +8,25 @@
 	https://github.com/Mullets-Gavin/DiceManager
 	Listed below is a quick glance on the API, visit the link above for proper documentation.
 	
-	Manager.set(dictionary)
+	... = parameters
+	
 	Manager.wait(time)
-	Manager.wrap(function)
-	Manager.spawn(function)
-	Manager.delay(time,function)
+	Manager.set(dictionary)
+	Manager.wrap(function,...)
+	Manager.spawn(function,...)
 	Manager.garbage(time,instance)
+	Manager.delay(time,function,...)
+	Manager.retry(time,function,...)
 	
 	event = Manager:Connect(function)
 	event:Disconnect()
-	event:Fire()
+	event:Fire(...)
 	
 	event = Manager:ConnectKey(key,function)
 	event:Disconnect()
-	event:Fire()
+	event:Fire(...)
 	
-	Manager:FireKey(key,parameters)
+	Manager:FireKey(key,...)
 	Manager:DisconnectKey(key)
 	
 	event = Manager:Task([fps])
@@ -33,12 +36,36 @@
 	event:Wait()
 	event:Enabled()
 	event:Disconnect()
+	
+[LICENSE]:
+	MIT License
+
+	Copyright (c) 2020 Mullet Mafia Dev
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
 --]]
 
 --// logic
 local Manager = {}
 Manager.Connections = {}
 Manager.LastIteration = nil
+Manager.Name = 'DiceManager'
 
 local Settings = {}
 Settings.Debug = false
@@ -49,6 +76,15 @@ local Services = setmetatable({}, {__index = function(cache, serviceName)
 	cache[serviceName] = game:GetService(serviceName)
 	return cache[serviceName]
 end})
+
+--// variables
+local Bindable = Services['ReplicatedStorage']:FindFirstChild(Manager.Name); do
+	if not Bindable then
+		Bindable = Instance.new('BindableEvent')
+		Bindable.Name = Manager.Name
+		Bindable.Parent = Services['ReplicatedStorage']
+	end
+end
 
 --// functions
 --[[
@@ -74,10 +110,12 @@ end
 function Manager.wait(clock)
 	if clock then
 		local current = os.clock()
+		
 		while clock > os.clock() - current do
 			Services['RunService'][Settings.RunService]:Wait()
 		end
 	end
+	
 	return Services['RunService'][Settings.RunService]:Wait()
 end
 
@@ -88,12 +126,14 @@ end
 ]]--
 function Manager.wrap(code,...)
 	assert(typeof(code) == 'function',"[DICE MANAGER]: 'wrap' only accepts functions, got '".. typeof(code) .."'")
-	local contents = table.unpack({...})
-	local event
-	event = Services['RunService'][Settings.RunService]:Connect(function()
-		event:Disconnect()
-		return code(contents)
-	end)
+	
+	local thread = coroutine.create(code)
+    local ran,response = coroutine.resume(thread, ...)
+   
+   if not ran then
+        local trace = debug.traceback(thread)
+        error(response .. "\n" .. trace)
+    end
 end
 
 --[[
@@ -101,18 +141,59 @@ end
 	
 	.spawn(function)
 ]]--
-function Manager.spawn(code)
+function Manager.spawn(code,...)
 	assert(typeof(code) == 'function',"[DICE MANAGER]: 'spawn' only accepts functions, got '".. typeof(code) .."'")
-	local event
-	event = Services['RunService'][Settings.RunService]:Connect(function()
+	
+	local data = {...}
+	local event; event = Services['RunService'][Settings.RunService]:Connect(function()
 		event:Disconnect()
+		
 		local success,err = pcall(function()
-			return code()
+			return code(table.unpack(data))
 		end)
+		
 		if not success and Settings.Debug then
 			warn(err)
 		end
+		
+		return
 	end)
+end
+
+--[[
+	Variations of call:
+	
+	.loop(fps,code)
+	.loop(code)
+	
+	Returns:
+	
+	RBXScriptSignal
+--]]
+function Manager.loop(...)
+	local data = {...}
+	local fps,code; do
+		if typeof(data[1]) == 'number' then
+			fps = data[1]
+			code = data[2]
+		else
+			fps = 60
+			code = data[1]
+		end
+	end
+	
+	local rate = 1/60
+	local logged = 0
+	local event; event = Services['RunService'][Settings.RunService]:Connect(function(delta)
+		logged = logged + delta
+		
+		while logged >= rate do
+			logged = logged - rate
+			code()
+		end
+	end)
+	
+	return event
 end
 
 --[[
@@ -120,14 +201,18 @@ end
 	
 	.delay(time,function)
 ]]--
-function Manager.delay(clock,code)
+function Manager.delay(clock,code,...)
 	assert(typeof(clock) == 'number' and typeof(code) == 'function',"[DICE MANAGER]: 'delay' missing parameters, got '".. typeof(clock) .."' and '".. typeof(code) .."'")
+	
+	local data = {...}
 	Manager.wrap(function()
 		local current = os.clock()
-		while clock < os.clock() - current do
+		
+		while clock > os.clock() - current do
 			Manager.wait()
 		end
-		return Manager.wrap(code)
+		
+		return Manager.wrap(code,table.unpack(data))
 	end)
 end
 
@@ -138,13 +223,39 @@ end
 ]]--
 function Manager.garbage(clock,obj)
 	assert(typeof(clock) == 'number' and typeof(obj) == 'Instance',"[DICE MANAGER]: 'garbage' missing parameters, got '".. typeof(clock) .."' and '".. typeof(Instance) .."'")
+	
 	Manager.wrap(function()
 		local current = os.clock()
-		while clock < os.clock() - current do
+		
+		while clock > os.clock() - current do
 			Manager.wait()
 		end
+		
 		obj:Destroy()
 	end)
+end
+
+--[[
+	Variations of call:
+	
+	.retry(timeout,function)
+--]]
+function Manager.retry(clock,code,...)
+	assert(typeof(clock) == 'number' and typeof(code) == 'function',"[DICE MANAGER]: 'retry' missing parameters, got '".. typeof(clock) .."' and '".. typeof(code) .."'")
+	
+	local current = os.clock()
+	local success,response
+	
+	while not success and clock > os.clock() - current do
+		success,response = pcall(code,...)
+		Manager.wait()
+	end
+	
+	if not success and Settings.Debug then
+		warn(response,debug.traceback())
+	end
+	
+	return success,response
 end
 
 --[[
@@ -165,6 +276,7 @@ function Manager:Connect(code)
 	
 	function control:Disconnect()
 		control = nil
+		
 		if typeof(code) == 'RBXScriptConnection' then
 			code:Disconnect()
 		elseif typeof(code) == 'table' then
@@ -175,6 +287,7 @@ function Manager:Connect(code)
 				warn('[DICE MANAGER]:',err)
 			end
 		end
+		
 		code = nil
 		setmetatable(control, {
 			__index = function()
@@ -220,6 +333,7 @@ function Manager:ConnectKey(key,code)
 	function control:Disconnect()
 		if not Manager.Connections[key] then return end
 		Manager.Connections[key][code] = nil
+		
 		if typeof(code) == 'RBXScriptConnection' then
 			code:Disconnect()
 		elseif typeof(code) == 'table' then
@@ -230,6 +344,7 @@ function Manager:ConnectKey(key,code)
 				warn('[DICE MANAGER]:',err)
 			end
 		end
+		
 		code = nil
 		setmetatable(control, {
 			__index = function()
@@ -261,6 +376,7 @@ end
 --]]
 function Manager:FireKey(key,...)
 	assert(key ~= nil,"[DICE MANAGER]: 'FireKey' missing parameters, got key '".. typeof(key) .."'")
+	
 	if Manager.Connections[key] then
 		for code,control in pairs(Manager.Connections[key]) do
 			control:Fire(...)
@@ -275,6 +391,7 @@ end
 --]]
 function Manager:DisconnectKey(key)
 	assert(key ~= nil,"[DICE MANAGER]: 'DisconnectKey' missing parameters, got key '".. typeof(key) .."'")
+	
 	if Manager.Connections[key] then
 		for code,control in pairs(Manager.Connections[key]) do
 			control:Disconnect()
@@ -322,9 +439,11 @@ function Manager:Task(targetFPS)
 	
 	local function Loop()
 		control.UpdateTableEvent = Services['RunService'][Settings.RunService]:Connect(Update)
+		
 		while (true) do
 			if control.Sleeping then break end
 			if not control:Enabled() then break end
+			
 			if targetFPS < 0 then
 				if (#control.CodeQueue > 0) then
 					control.CodeQueue[1]()
@@ -354,6 +473,7 @@ function Manager:Task(targetFPS)
 				end
 			end
 		end
+		
 		control.UpdateTableEvent:Disconnect()
 		control.UpdateTableEvent = nil
 	end
@@ -365,6 +485,7 @@ function Manager:Task(targetFPS)
 	function control:Pause()
 		control.Paused = true
 		control.Sleeping = true
+		
 		local fps = (((os.clock() - start) >= 1 and #control.UpdateTable) or (#control.UpdateTable / (os.clock() - start)))
 		return fps
 	end
@@ -375,6 +496,7 @@ function Manager:Task(targetFPS)
 			control.Sleeping = false
 			Loop()
 		end
+		
 		local fps = (((os.clock() - start) >= 1 and #control.UpdateTable) or (#control.UpdateTable / (os.clock() - start)))
 		return fps
 	end
@@ -383,6 +505,7 @@ function Manager:Task(targetFPS)
 		while not control.Sleeping do
 			Manager.wait()
 		end
+		
 		local fps = (((os.clock() - start) >= 1 and #control.UpdateTable) or (#control.UpdateTable / (os.clock() - start)))
 		return fps
 	end
@@ -394,9 +517,11 @@ function Manager:Task(targetFPS)
 		control.UpdateTable = nil
 		control.UpdateTableEvent:Disconnect()
 		control:Wait()
+		
 		for index in pairs(control) do
 			control[index] = nil
 		end
+		
 		setmetatable(control, {
 			__index = function()
 				error('[DICE MANAGER]: Attempt to use destroyed task scheduler')
@@ -410,6 +535,7 @@ function Manager:Task(targetFPS)
 	function control:Queue(code)
 		if not control.CodeQueue then return end
 		control.CodeQueue[#control.CodeQueue + 1] = code
+		
 		if (control.Sleeping and not control.Paused) then
 			control.Sleeping = false
 			Loop()
